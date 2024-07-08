32adba78ff739d02fae031a75552323f7bb268e7
subhajit roy <rsubhajit024@gmail.com>
2024-07-08 13:36:00 +0530

    connect to mongodb atlas

--- src/index.js
+++ src/index.js
@@ -31,8 +31,6 @@ connectDB()
         app.listen(process.env.PORT,()=>{
             console.log(`app is listening on port ${process.env.PORT}`)
         })
-
-
     }
     catch(error){
         console.log("ERROR: ",error);
